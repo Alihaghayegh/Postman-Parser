ae1c2cf709416a903281924f21198df0db02342e
ali haghayegh <haghayeghali95@gmail.com>
2022-12-05 14:17:47 +0330

    turning to class base and updating README

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 - This module opens the Postman Collection json and parse it and return the cURL
 Pass the collection path to main function, and it returns a list of curls.
 ***********************************************************************************
+* At first create an instance of Postman_parser class :
+```python
+postman = Postman_parser()
+```
+* Postman_parser class has method called postman_to_curl .
+```python
+curls = postman.postman_to_curl("your postman collection address")
+```
+* Pass the complete Postman collection address on your machine and this method returns a list of curl commands.
+```python
+print(curls)
+```
--- main.py
+++ main.py
@@ -6,33 +6,37 @@ Pass the collection path to main function, and it returns a list of curls
 import json
 
 
-def postman_parser(collection):
-    output = {}
-    curls = []
-    with open(f"{collection}", "rb") as file:
-        f = json.load(file)
-        items = f.get("item")
-        variables = f.get("variable")
-        output['names'] = [item['name'] for item in items]
-        output['request'] = [item['request'] for item in items]
+class Postman_Parser:
+    def __init__(self):
+        self.output = {}
+        self.curls = []
 
-        for out in output['request']:
-            key = out['url']['raw'].split("/")[0]
-            for variable in variables:
-                if key[2:-2] == variable['key']:
-                    url = out['url']['raw'].replace(key, variable['value'])
-                    c = f"curl -X {out['method']} \"{url}\" "
-                else:
-                    c = f"curl -X {out['method']} \"{out['url']['raw']}\" "
-            for header in out['header']:
-                header_key = header.get('key')
-                header_value = header.get('value')
-                headers = f"-H \"{header_key}: {header_value}\" "
-                c = c + headers
+    def postman_to_curl(self, collection):
 
-            if out['method'] != 'GET':
-                body = out.get('body', "")
-                raw = body.get('raw', '') if body else '{}'
-                c = c + f"-d \"{raw}\" "
-            curls.append(c)
-        return curls
+        with open(f"{collection}", "rb") as file:
+            f = json.load(file)
+            items = f.get("item")
+            variables = f.get("variable")
+            self.output['names'] = [item['name'] for item in items]
+            self.output['request'] = [item['request'] for item in items]
+
+            for out in self.output['request']:
+                key = out['url']['raw'].split("/")[0]
+                for variable in variables:
+                    if key[2:-2] == variable['key']:
+                        url = out['url']['raw'].replace(key, variable['value'])
+                        c = f"curl -X {out['method']} \"{url}\" "
+                    else:
+                        c = f"curl -X {out['method']} \"{out['url']['raw']}\" "
+                for header in out['header']:
+                    header_key = header.get('key')
+                    header_value = header.get('value')
+                    headers = f"-H \"{header_key}: {header_value}\" "
+                    c = c + headers
+
+                if out['method'] != 'GET':
+                    body = out.get('body', "")
+                    raw = body.get('raw', '') if body else '{}'
+                    c = c + f"-d \"{raw}\" "
+                self.curls.append(c)
+            return self.curls
